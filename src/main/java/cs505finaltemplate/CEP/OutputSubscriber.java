0065910dbad4dc4f3f9270c4bbae226a0ecf4146
KolibreKrit <84860454+KolibreKrit@users.noreply.github.com>
2022-04-19 15:38:43 -0400

    Basics

--- src/main/java/cs505finaltemplate/CEP/OutputSubscriber.java
+++ src/main/java/cs505finaltemplate/CEP/OutputSubscriber.java
@@ -33,20 +33,20 @@ public class OutputSubscriber implements InMemoryBroker.Subscriber {
 
             //You will need to parse output and do other logic,
             //but this sticks the last output value in main
-            List<ZipData> incomingList = gson.fromJson(String.valueOf(msg), typeListZipData);
-            Launcher.alerts = new ArrayList<>();
-            for (ZipData zipData : incomingList) {
-                for (ZipData prevData : Launcher.CEPList) {
-                    for (ZipInfo event : zipData.events) {
-                        for (ZipInfo prevEvent : prevData.events) {
-                            if (event.zip_code.equals(prevEvent.zip_code)) {
-                                Launcher.alerts.add(event.zip_code);
-                            }
-                        }
-                    }
-                }
-            }
-            Launcher.CEPList = incomingList;
+//            List<ZipData> incomingList = gson.fromJson(String.valueOf(msg), typeListZipData);
+//            Launcher.alerts = new ArrayList<>();
+//            for (ZipData zipData : incomingList) {
+//                for (ZipData prevData : Launcher.CEPList) {
+//                    for (ZipInfo event : zipData.events) {
+//                        for (ZipInfo prevEvent : prevData.events) {
+//                            if (event.zip_code.equals(prevEvent.zip_code)) {
+//                                Launcher.alerts.add(event.zip_code);
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//            Launcher.CEPList = incomingList;
             Launcher.lastCEPOutput = String.valueOf(msg);
 
             //String[] sstr = String.valueOf(msg).split(":");
