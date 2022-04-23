0a3ffb896dc81060c0a0d664006746887be651db
KolibreKrit <84860454+KolibreKrit@users.noreply.github.com>
2022-04-23 18:43:48 -0400

    Basics

--- src/main/java/cs505finaltemplate/Topics/TopicConnector.java
+++ src/main/java/cs505finaltemplate/Topics/TopicConnector.java
@@ -107,10 +107,12 @@ public class TopicConnector {
 //                    Launcher.graphDBEngine.createPatient(testingData.patient_mrn);
                     if (Launcher.graphDBEngine.isPatient(testingData.patient_mrn)) {
                         OVertex patient_1 = Launcher.graphDBEngine.getPatient(testingData.patient_mrn);
-                        for (String contact : testingData.contact_list) {
-                            if (!Launcher.graphDBEngine.isPatient(contact)) {
-                                OVertex patient_2 = Launcher.graphDBEngine.createPatient(contact);
-                                Launcher.graphDBEngine.createContact(patient_1, patient_2);
+                        if (testingData.contact_list != null) {
+                            for (String contact : testingData.contact_list) {
+                                if (!Launcher.graphDBEngine.isPatient(contact)) {
+                                    OVertex patient_2 = Launcher.graphDBEngine.createPatient(contact);
+                                    Launcher.graphDBEngine.createContact(patient_1, patient_2);
+                                }
                             }
                         }
                     }
