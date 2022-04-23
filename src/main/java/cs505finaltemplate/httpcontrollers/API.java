309860726d2c6ba1bc5933f6817ae177dd15ac28
KolibreKrit <84860454+KolibreKrit@users.noreply.github.com>
2022-04-23 13:00:22 -0400

    Basics

--- src/main/java/cs505finaltemplate/httpcontrollers/API.java
+++ src/main/java/cs505finaltemplate/httpcontrollers/API.java
@@ -7,6 +7,7 @@ import jdk.nashorn.internal.objects.annotations.Getter;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
