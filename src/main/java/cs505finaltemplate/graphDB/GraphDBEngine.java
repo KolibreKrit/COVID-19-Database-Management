027ec651943ae5efa83c96f15f33c9ea58d7f8e3
KolibreKrit <84860454+KolibreKrit@users.noreply.github.com>
2022-04-23 17:39:43 -0400

    Basics

--- src/main/java/cs505finaltemplate/graphDB/GraphDBEngine.java
+++ src/main/java/cs505finaltemplate/graphDB/GraphDBEngine.java
@@ -95,7 +95,7 @@ public class GraphDBEngine {
 
     public boolean isPatient(String patient_mrn) {
         String query = "select from patient where patient_mrn = " + patient_mrn;
-        OResultSet rs = db.execute(query);
+        OResultSet rs = db.query(query);
 
         System.out.println("Name: " + db.getName());
         while (rs.hasNext()) {
@@ -112,7 +112,7 @@ public class GraphDBEngine {
 
     public OVertex getPatient(String patient_mrn) {
         String query = "select from patient where patient_mrn = " + patient_mrn;
-        OResultSet rs = db.execute(query);
+        OResultSet rs = db.query(query);
 
         OVertex patient = null;
 
